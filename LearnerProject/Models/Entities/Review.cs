076374f3ba1c878d677ae18ae1d9f39c293c5fa1
ieneskeloglu <ieneskeloglu2@outlook.com>
2024-04-22 04:39:38 +0300

    Add project files.

---
+++ LearnerProject/Models/Entities/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnerProject.Models.Entities
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        public double ReviewValue { get; set; }
+        public string Comment { get; set; }
+
+        public int StudentId { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
